15c14c87c5c9969e40a3713ef6e09b9cfcf02831
Quasel <github@just4us.eu>
2017-05-09 12:13:37 +0200

    chnanged all $key to underscores

--- includes/pods-page-data.php
+++ includes/pods-page-data.php
@@ -6,7 +6,7 @@
 $data = array(
 	'label'       => __( 'Display Field (Dropdown)', 'fl-theme-builder' ),
 	'group'       => 'pods',
-	'type'        => array( 'string', 'html', 'url' ),
+	'type'        => array( 'string', 'html' ),
 	'getter'      => 'PodsPageData::get_field_display',
 	'placeholder' => __( 'Lorem Ipsum ...', 'fl-theme-builder' )
 );
@@ -25,8 +25,8 @@ $form = array(
 
 // $form = PodsPageData::pods_settings_form();
 
-FLPageData::add_post_property( 'pods', $data );
-FLPageData::add_post_property_settings_fields( 'pods', $form );
+FLPageData::add_post_property( 'pods_display', $data );
+FLPageData::add_post_property_settings_fields( 'pods_display', $form );
 
 
 /**
@@ -78,8 +78,8 @@ $form = array(
 );
 
 
-FLPageData::add_post_property( 'pods-multiple-photos', $data );
-FLPageData::add_post_property_settings_fields( 'pods-multiple-photos', $form );
+FLPageData::add_post_property( 'pods_multiple_photos', $data );
+FLPageData::add_post_property_settings_fields( 'pods_multiple_photos', $form );
 
 /**
  * Pods Photos (Images)
@@ -104,33 +104,7 @@ $form = array(
 	)
 );
 
-FLPageData::add_post_property( 'pods-photos', $data );
-FLPageData::add_post_property_settings_fields( 'pods-photos', $form );
+FLPageData::add_post_property( 'pods_photos', $data );
+FLPageData::add_post_property_settings_fields( 'pods_photos', $form );
 
-/**
- * Pods String & HTML
- */
-$data = array(
-	'label'       => __( 'Display Field (Dropdown)', 'fl-theme-builder' ),
-	'group'       => 'pods',
-	'type'        => array( 'string', 'html', 'url' ),
-	'getter'      => 'PodsPageData::get_field_display',
-	'placeholder' => __( 'Lorem Ipsum ...', 'fl-theme-builder' )
-);
 
-$form = array(
-	'field' => array(
-		'type'        => 'select',
-		'label'       => __( 'Field Name (CPT):', 'fl-theme-builder' ),
-		'options'     => 'PodsPageData::pods_get_fields',
-		'help'        => __( 'If two CPT have a field with the same name - the field is only listed once - this way you could use the same Template for diffrent CPT if they use the same field names.)', 'fl-theme-builder' ),
-		'description' => __( 'Select one', 'fl-theme-builder' ),
-		'placeholder' => __( 'Field Name (CPT)', 'fl-theme-builder' )
-
-	)
-);
-
-// $form = PodsPageData::pods_settings_form();
-
-FLPageData::add_post_property( 'pods', $data );
-FLPageData::add_post_property_settings_fields( 'pods', $form );
