0bc48ffd9cee2d18a20a10e80b3818da3079d2e4
Scott Kingsley Clark <lol@scottkclark.com>
2017-06-21 16:54:44 -0500

    Update formatting, change delegate to on, optimize JS var usage a little

--- assets/js/settings-form.js
+++ assets/js/settings-form.js
@@ -1,18 +1,19 @@
-(function($) {
-    $(function() {
-        $('body').delegate('.fl-builder-settings-fields select[name="use_pods"]', 'change', function (e) {
-            var parentTab               = $(this).closest('#fl-builder-settings-tab-content');
-            var sourceSelectField       = $(parentTab).find('select[name="data_source"]');
-            var sourceSelectFieldParent = $(sourceSelectField).closest('tbody');
+( function( $ ) {
+    $( function() {
+        $( 'body' ).on( 'change', '.fl-builder-settings-fields select[name="use_pods"]', function ( e ) {
+            var $parentTab               = $( this ).closest( '#fl-builder-settings-tab-content' );
+            var $sourceSelectField       = $parentTab.find( 'select[name="data_source"]' );
+            var $sourceSelectFieldParent = $sourceSelectField.closest( 'tbody' );
 
-            if (e.target.value !== 'no') {
-                $(sourceSelectField).val('custom_query').trigger('change');
+            if ( 'no' !== e.target.value ) {
+                $sourceSelectField.val( 'custom_query' );
+				$sourceSelectField.trigger( 'change' );
 
-                $(sourceSelectFieldParent).hide();
+                $sourceSelectFieldParent.hide();
             }
             else {
-                $(sourceSelectFieldParent).show();
+                $sourceSelectFieldParent.show();
             }
-        });
-    });
-})(jQuery);
+        } );
+    } );
+} )( jQuery );
